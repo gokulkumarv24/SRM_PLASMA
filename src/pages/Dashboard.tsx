8eb85762ab0e55552d273e74b24c660ffc61c895
GOKUL KUMAR V <128581860+gokulkumarv24@users.noreply.github.com>
2025-07-23 22:52:20 +0530

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,7 +20,7 @@ import {
   getDepartmentStats,
   getMonthlyPlacements,
   getCompanyWiseData,
-  getPackageDistributio
+  getPackageDistribution,
   getStatusDistribution,
 } from '../utils/chartUtils';
 import { Student } from '../types';
